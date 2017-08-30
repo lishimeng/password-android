faac95c8a120eef1d74e9d16f246dcbf822ffd42
lishimeng <alex.li@jxfinancial.cn>
2017-08-30 17:41:02 +0800

    init

---
+++ src/main/java/com/thingple/keyboard/password/adapter/ViewHolder.java
@@ -0,0 +1,12 @@
+package com.thingple.keyboard.password.adapter;
+
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * 存放控件
+ */
+class ViewHolder {
+    TextView btnKey;
+    RelativeLayout imgDelete;
+}
---
+++ src/main/java/com/thingple/keyboard/password/widget/PopEnterPassword.java
@@ -0,0 +1,87 @@
+package com.thingple.keyboard.password.widget;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.PopupWindow;
+
+import com.thingple.keyboard.password.PasswordInputListener;
+import com.thingple.keyboard.password.R;
+
+
+/**
+ * 输入密码<br/>
+ *
+ * Created by lism on 2017/8/29.
+ */
+public class PopEnterPassword extends PopupWindow {
+
+    private PasswordView pwdView;
+
+    private View mMenuView;
+
+    private Activity mContext;
+
+    public PopEnterPassword(final Activity context) {
+
+        super(context);
+        this.mContext = context;
+
+        initComponents();
+    }
+
+    /**
+     * 密码输入完成的响应
+     * @param listener listener
+     */
+    public void setPasswordInputListener(PasswordInputListener listener) {
+        pwdView.setPasswordInputListener(listener);
+    }
+
+    private void initComponents() {
+
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        mMenuView = inflater.inflate(R.layout.pop_enter_password, null);
+
+        pwdView = (PasswordView) mMenuView.findViewById(R.id.pwd_view);
+
+        // 监听X关闭按钮
+        pwdView.getImgCancel().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        // 监听键盘上方的返回
+        pwdView.getVirtualKeyboardView().getLayoutBack().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        settingComponents();
+
+    }
+
+    private void settingComponents() {
+        // 设置SelectPicPopupWindow的View
+        this.setContentView(mMenuView);
+        // 设置SelectPicPopupWindow弹出窗体的宽
+        this.setWidth(LayoutParams.MATCH_PARENT);
+        // 设置SelectPicPopupWindow弹出窗体的高
+        this.setHeight(LayoutParams.WRAP_CONTENT);
+        // 设置SelectPicPopupWindow弹出窗体可点击
+        this.setFocusable(true);
+        // 设置SelectPicPopupWindow弹出窗体动画效果
+        this.setAnimationStyle(R.style.pop_add_ainm);
+        // 实例化一个ColorDrawable颜色为半透明
+        ColorDrawable dw = new ColorDrawable(0x66000000);
+        // 设置SelectPicPopupWindow弹出窗体的背景
+        this.setBackgroundDrawable(dw);
+    }
+}
